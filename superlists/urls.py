40a567482ebdf30b8d8eaa320ffbfbe3820263a2
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-09-13 11:20:50 -0700

    add home page url

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lists import views
 
 urlpatterns = [
+	path('', views.home_page, name='home'),
     path('admin/', admin.site.urls),
 ]
