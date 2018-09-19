a134373e5498c868428cd833457349c01735f635
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-09-18 21:24:18 -0700

    break out urls by app

--- lists/urls.py
+++ lists/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, re_path
 from lists import views
 
 urlpatterns = [
-	path('', views.home_page, name='home'),
-	path('lists/', include('lists.urls')),
-    path('admin/', admin.site.urls),
+	path('new', views.new_list, name='new_list'),
+	re_path(r'^(\d+)/$', views.view_list, name='view_list'),
+	re_path(r'^(\d+)/add_item$', views.add_item, name='add_item'),
 ]
