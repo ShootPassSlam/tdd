8ee46ec5107cd5a2cff712e7f1ade2230ab76030
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-09-20 17:08:05 -0700

    readded admin and allowing staging server to have tests run on it

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,13 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+import os
 
 MAX_WAIT = 10 
 
-class NewVisitorTest(StaticLiveServerTestCase): 
+class NewVisitorTest(StaticLiveServerTestCase):
 
-    def setUp(self):  
+    def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')  
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server  
 
     def tearDown(self):  
         self.browser.quit()
@@ -139,5 +143,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
-
-        self.fail('Finish the test!')
