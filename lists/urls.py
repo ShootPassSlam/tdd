b3d5a4315ca2c40a2bdb3f6283bebb9373a0c61f
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-09-24 18:06:44 -0700

    Refactor list view to handle new item POSTs

--- lists/urls.py
+++ lists/urls.py
@@ -20,5 +20,4 @@ from lists import views
 urlpatterns = [
 	path('new', views.new_list, name='new_list'),
 	re_path(r'^(\d+)/$', views.view_list, name='view_list'),
-	re_path(r'^(\d+)/add_item$', views.add_item, name='add_item'),
 ]
--- lists/views.py
+++ lists/views.py
@@ -10,6 +10,9 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect(f'/lists/{list_.id}/')
     return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
@@ -22,9 +25,4 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
-    return redirect(f'/lists/{list_.id}/')
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
