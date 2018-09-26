19adad7b2e3cb74e9f97104aa60b667c264472d5
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-09-26 16:24:00 -0700

    custom error message for duplicate items in list

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -9,6 +9,8 @@ from lists.views import home_page
 from lists.models import Item, List
 from lists.forms import ItemForm
 
+from unittest import skip
+
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
@@ -95,6 +97,20 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)        
 
     def test_displays_item_form(self):
         list_ = List.objects.create()
