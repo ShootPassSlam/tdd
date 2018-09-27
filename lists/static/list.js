3cfe5bc01575551dc324d59dbeec924b2f832a96
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-09-27 16:42:08 -0700

    trigger clearing the error when anything is typed

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,9 @@
-var initialize = function () {
-  $('input[name="text"]').on('keypress', function () {
+window.Superlists = {}; 
+window.Superlists.initialize = function () { 
+  $('input[name="text"]').on('propertychange change click keyup input paste', function () {
+    $('.has-error').hide();
+  });
+  $('input[name="text"]').click(function () {
     $('.has-error').hide();
   });
 };
