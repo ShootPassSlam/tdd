8ed6465fff023c4bc61ac1b2b50981f32fdaea90
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-10-02 20:28:02 -0700

    spiked passwordless custom auth backed

--- superlists/settings.py
+++ superlists/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
+]
+
+AUTH_USER_MODEL = 'accounts.ListUser'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +129,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#for email sending
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'dominicscotto@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
