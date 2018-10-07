6b26237099db8585c242845c64ebe12bc4458872
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-10-07 14:49:05 -0700

    add pages to view a users lists

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,4 +1,5 @@
 import random
+import os
 from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
 
@@ -28,14 +29,16 @@ def _update_virtualenv():
     run('./virtualenv/bin/pip install -r requirements.txt')
 
 def _create_or_update_dotenv():
-    append('.env', 'DJANGO_DEBUG_FALSE=y')  
+    append('.env', 'DJANGO_DEBUG_FALSE=y')
     append('.env', f'SITENAME={env.host}')
-    current_contents = run('cat .env')  
-    if 'DJANGO_SECRET_KEY' not in current_contents:  
-        new_secret = ''.join(random.SystemRandom().choices(  
+    current_contents = run('cat .env')
+    if 'DJANGO_SECRET_KEY' not in current_contents:
+        new_secret = ''.join(random.SystemRandom().choices(
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
         ))
         append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+    email_password = os.environ['EMAIL_PASSWORD']  
+    append('.env', f'EMAIL_PASSWORD={email_password}')
 
 def _update_static_files():
     run('./virtualenv/bin/python manage.py collectstatic --noinput')
--- lists/urls.py
+++ lists/urls.py
@@ -20,4 +20,5 @@ from lists import views
 urlpatterns = [
 	path('new', views.new_list, name='new_list'),
 	re_path(r'^(\d+)/$', views.view_list, name='view_list'),
+    re_path(r'^users/(.+)/$', views.my_lists, name='my_lists'),
 ]
