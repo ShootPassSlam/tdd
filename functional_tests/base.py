102c297f74e880130509ccbc55f55e97a9c9ae9b
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-10-06 23:10:37 -0700

    fabric commands to create sessions on the server

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,6 +2,7 @@ import os
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from .server_tools import reset_database
 import time
 
 MAX_WAIT = 10 
@@ -25,6 +26,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)  
 
     def tearDown(self):  
         self.browser.quit()
